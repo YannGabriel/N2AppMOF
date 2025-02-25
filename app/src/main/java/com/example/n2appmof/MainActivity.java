43812d3754b13f55777f6abc77294b95d5cdd43e
Yann Gabriel <yanngabrieldev@gmail.com>
2025-02-24 23:49:32 -0300

    put first imagem in android studio (import)

--- app/src/main/java/com/example/n2appmof/MainActivity.java
+++ app/src/main/java/com/example/n2appmof/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.n2appmof;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+
 }
