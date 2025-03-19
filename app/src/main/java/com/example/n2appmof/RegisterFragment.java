0f0201ffd5eda15d122ea240bdf632239419017c
Yann Gabriel <yanngabrieldev@gmail.com>
2025-03-19 16:09:26 -0300

    Create a modal and add fonts (Poppins -Bold - Regular - Light- ) for the app

--- app/src/main/java/com/example/n2appmof/RegisterFragment.java
+++ app/src/main/java/com/example/n2appmof/RegisterFragment.java
@@ -1,7 +1,20 @@
 package com.example.n2appmof;
-
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import android.widget.Button;
 
 public class RegisterFragment extends BottomSheetDialogFragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.register_fragment, container, false);
+
+        Button createUser = view.findViewById(R.id.createUserButton);
+        createUser.setOnClickListener(v -> dismiss());
+
+        return view;
+    }
 }
